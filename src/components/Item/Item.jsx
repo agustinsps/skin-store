693c56b2c2e9501f147daa823447a26e11fd6ce5
Agustin Sparks <agustinsparks@gmail.com>
2021-08-14 20:57:47 -0300

    No salio como esperaba

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,13 +1,94 @@
 import React from "react";
 import "./ItemStyle.css";
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import CartWidget from "../CartWidget/CartWidget";
 
-function Item({name, price}) {
-    return (
-        <div className="styleCard">
-            <div>{name}</div>
-            <div>{price}</div>
-        </div>
-    );
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        maxWidth: 345,
+    },
+    action: {
+        marginTop: "1rem",
+    },
+    media: {
+        height: 0,
+        paddingTop: '56.25%', // 16:9
+    },
+    expand: {
+        transform: 'rotate(0deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+        }),
+    },
+    expandOpen: {
+        transform: 'rotate(180deg)',
+    },
+}));
+
+function Item ({ name, productType, price, image, description }) {
+    const classes = useStyles();
+    const [expanded, setExpanded] = React.useState(false);
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+
+        return (
+            <Card className={classes.root}>
+                <CardHeader>
+                    action = {
+                        <Typography
+                            className={classes.action}
+                            variant='h5'
+                            color='textSecondary'
+                        >
+                           <div> {price} </div>
+                        </Typography>
+                       }
+                   title = {name}
+                </CardHeader>
+                <CardMedia className={classes.media}>
+                    <div> {image} </div>
+                <CardMedia />
+                    <CardContent>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                          <div> {productType} </div> 
+                        </Typography>
+                    </CardContent>
+                    <CardActions disableSpacing>
+                        <IconButton>
+                            <CartWidget />
+                        </IconButton>
+                        <IconButton
+                            className={clsx(classes.expand, {
+                                [classes.expandOpen]: expanded,
+                            })}
+                            onClick={handleExpandClick}
+                            aria-expanded={expanded}
+                            aria-label="show more"
+                        >
+                            <ExpandMoreIcon />
+                        </IconButton>
+                    </CardActions>
+                    <Collapse in={expanded} timeout="auto" unmountOnExit>
+                        <CardContent>
+                            <Typography> <div> {description} </div> </Typography>
+                        </CardContent>
+                    </Collapse>
+            </Card>
+                );
   }
 
-  export default Item
+                export default Item
--- src/components/Item/ItemLists.jsx
+++ src/components/Item/ItemLists.jsx
@@ -5,29 +5,40 @@ import Item from "./Item";
 function ItemLists() {
   const items = [
     {
-      id: "1",
-      name: "AK-47",
-      price: "11",
+      id: 1,
+      name: "Ak-47",
+      productType: "Fusil de asalto",
+      price: 40,
+      image: <img src="Images/Ak-47.png" alt="Ak-47" />,
+      description:
+        "El Avtomat Kalashnikova modelo 1947 o más conocido por el acrónimo AK-47 es un fusil de asalto soviético, de calibre 7,62 mm, diseñado por Mijaíl Kaláshnikov, combatiente soviético durante la Segunda Guerra Mundial. Fue el fusil oficial de la Unión Soviética entre 1951 y 1978.",
     },
     {
-      id: "2",
-      name: "HK416",
-      price: "14",
+      id: 2,
+      name: "M4-16",
+      productType: "Carabina",
+      price: 35,
+      image: <img src="Images/M416.png" alt="M416" />,
+      description:
+        "El HK416 es una carabina diseñada y manufacturada por Heckler & Koch. Es una versión mejorada de la carabina M4, con muchos cambios con respecto a esta, más notablemente su sistema de gases, similar al del HK G36, en calibre 5,56 × 45 mm OTAN. Los clientes de fuerzas militares y policiales pueden comprar la parte de la caja de mecanismos, amortiguador y sistema de gases para reacondicionar fusiles AR-15 o bien comprar el arma completa.",
     },
     {
       id: "3",
       name: "Arctic Warfare",
       price: "18",
-    }
-  ]
+    },
+  ];
 
-const ItemList = items.map (item => <Item name={item.name} price={item.price}/>)
+  const ItemList = items.map((item) => (
+    <Item
+      name={item.name}
+      productType={item.productType}
+      price={item.price}
+      image={item.image}
+    />
+  ));
 
-  return (
-    <>
-      {ItemList}
-    </>
-  );
+  return <>{ItemList}</>;
 }
 
 export default ItemLists;
